6457d5910ec8df71d7b99c263ffcc11bffb25aa9
marshall2023 <74441675+michaelfan0310@users.noreply.github.com>
2022-04-07 12:24:06 -0400

    e2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const DUMMY_EXPENSES = [
     },
     { id: 'e2', title: 'New TV', 
       amount: 799.49, 
-      date: new Date(2021, 2, 12) },
+      date: new Date(2021, 2, 15) },
     {
       id: 'e3',
       title: 'Car Insurance',
